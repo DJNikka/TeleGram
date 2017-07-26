6308391d3e145acacf735275cf715779cbe96022
Nikka-Sher <npiterman@gmail.com>
2017-07-26 12:41:04 -0700

    log in button class

---
+++ TeleGram/RoundedButton.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedButton.swift
+//  TeleGram
+//
+//  Created by Konstantine Piterman on 7/26/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        //default value is 0
+        
+        didSet{
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+            
+            //if corner is rounded at all, turn on bordering
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+
+}
