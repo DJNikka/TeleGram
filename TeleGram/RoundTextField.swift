72de36b91f708dcc94c1130e8a05dd777a26a035
Nikka-Sher <npiterman@gmail.com>
2017-07-26 11:42:56 -0700

    RoundTextField, IBInspectable

---
+++ TeleGram/RoundTextField.swift
@@ -0,0 +1,15 @@
+//
+//  RoundTextField.swift
+//  TeleGram
+//
+//  Created by Konstantine Piterman on 7/26/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundTextField: UITextField {
+
+
+}
