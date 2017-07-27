951a50b56554d7d9196432820101f8ee9f21b997
Nikka-Sher <npiterman@gmail.com>
2017-07-26 23:51:01 -0700

    camera integrated with IB outlets, buttons. login messages

---
+++ TeleGram/AuthService.swift
@@ -0,0 +1,20 @@
+//
+//  AuthService.swift
+//  TeleGram
+//
+//  Created by Konstantine Piterman on 7/26/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import Foundation
+
+class AuthService {
+    private static let _instance = AuthService()
+    
+    static var instance: AuthService {
+        return _instance
+    }
+    func login(email: String, password: String) {
+    
+    }
+}
--- TeleGram/LoginVC.swift
+++ TeleGram/LoginVC.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var emailField: RoundTextField!
+    @IBOutlet weak var passwordField: RoundTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginPressed(_ sender: Any) {
+        if let email = emailField.text, let pass = passwordField.text, (email.characters.count > 0 && pass.characters.count > 0) {
+            
+            //call the login service
+        } else {
+        let alert = UIAlertController(title: "Username and Password Required", message: "You must enter both a username and a password", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alert, animated: true, completion:nil)
+        }
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
