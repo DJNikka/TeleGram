e6e3219a55aa41e060ccd2a9ded77e421e49b471
Nikka-Sher <npiterman@gmail.com>
2017-07-27 18:01:49 -0700

    users view controller, table view, cell, search bar UI

---
+++ TeleGram/UserCell.swift
@@ -0,0 +1,26 @@
+//
+//  UserCell.swift
+//  TeleGram
+//
+//  Created by Konstantine Piterman on 7/27/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var firstNameLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TeleGram/UsersVC.swift
@@ -0,0 +1,20 @@
+//
+//  UsersVC.swift
+//  TeleGram
+//
+//  Created by Konstantine Piterman on 7/27/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+class UsersVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
